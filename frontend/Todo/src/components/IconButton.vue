fdccd62015f39fe1120b5e917dcd659f85af97ca
Vinícius Machado de Souza <vinimadesou2@gmail.com>
2025-10-01 17:24:31 -0300

    Refatoração dos botões com ícone (#7)
    * Altera nome de arquivo e retira comentários
    
    * botão genérico
    
    * alteração
    
    * melhora dos ícones dos botões genéricos

---
+++ frontend/Todo/src/components/IconButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps({
+  extraClass: { type: [String, Object, Array], default: '' },
+  title: { type: String, default: '' },
+})
+
+const emit = defineEmits(['click'])
+
+function onClick(e: Event) {
+  emit('click', e)
+}
+</script>
+
+<template>
+  <button
+    :class="['focus:outline-none hover:cursor-pointer', extraClass]"
+    @click="onClick"
+    :title="title"
+  >
+    <slot />
+  </button>
+</template>
