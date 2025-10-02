24f83b4e00d77128c897629b0e5efc7f1e73690e
Vinícius Machado de Souza <vinimadesou2@gmail.com>
2025-10-02 12:07:16 -0300

    alteração (#9)

--- frontend/Todo/src/components/LogoLeds.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="bg-[#e2e3eb] mb-8 flex justify-center w-fit rounded-3xl">
-    <img src="@/assets/logo-leds.png" alt="Logo" class="h-16 w-auto" />
-  </div>
-</template>
--- frontend/Todo/src/components/VazioTodo.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="text-center font-semibold text-lg text-[#f5f5f5]">Você ainda não adicionou nenhuma tarefa.</div>
-</template>
