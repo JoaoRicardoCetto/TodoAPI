d6c5898722a02291183e104dd45c1e243fc9da9c
MachadoVsouza <vinimadesou2@gmail.com>
2025-10-02 12:06:26 -0300

    alteração

--- frontend/Todo/src/App.vue
+++ frontend/Todo/src/App.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import SpinnerTodo from './components/SpinnerTodo.vue'
 import FormTodo from './components/FormTodo.vue'
 import ListaTodo from './components/ListaTodo.vue'
-import VazioTodo from './components/VazioTodo.vue'
-import LogoLeds from './components/LogoLeds.vue'
 
 import './assets/main.css'
 import { onMounted, ref } from 'vue'
@@ -25,16 +22,22 @@ onMounted(async () => {
 
 <template>
   <div class="min-h-screen flex flex-col items-center justify-center bg-[#e2e3eb] px-4">
-    <LogoLeds />
+    <div class="bg-[#e2e3eb] mb-8 flex justify-center w-fit rounded-3xl">
+      <img src="@/assets/logo-leds.png" alt="Logo" class="h-16 w-auto" />
+    </div>
     <div class="w-full max-w-md flex flex-col items-center">
       <FormTodo class="w-full" />
       <div class="w-full mt-2">
         <template v-if="isLoading">
-          <SpinnerTodo />
+          <div class="flex justify-center items-center h-24">
+            <img src="@/assets/spinner.svg" alt="Loading..." class="w-8 h-8 animate-spin" />
+          </div>
         </template>
         <template v-else>
           <ListaTodo v-if="todoStore.getTodos.length > 0" />
-          <VazioTodo v-else />
+          <div v-else class="text-center font-semibold text-lg text-[#211b15]">
+            Você ainda não adicionou nenhuma tarefa.
+          </div>
         </template>
       </div>
     </div>
--- frontend/Todo/src/components/SpinnerTodo.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="flex justify-center items-center h-24">
-    <img src="@/assets/spinner.svg" alt="Loading..." class="w-8 h-8 animate-spin" />
-  </div>
-</template>
